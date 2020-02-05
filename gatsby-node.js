e7f0cd774a026445a3ce52788e5fb83ee03f54c3
Abdul <abdul.diaz@fcb.com>
2020-02-05 00:29:26 -0500

    added blog code for gatsby setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions: { createNodeField } }) => {
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions: { createPage } }) => graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`).then(result =>
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => createPage({
+    path: node.fields.slug,
+    component: path.resolve(`./src/templates/blog-post.js`),
+    context: {
+      slug: node.fields.slug
+    }
+  })),
+)
